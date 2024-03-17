1a95201ee0867bbbd28d23ebe9f23fadd4389dc1
Reutkap <126346601+reutkap@users.noreply.github.com>
2024-03-17 09:12:27 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,30 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
+import java.util.*;
+public class Bagrot {
+	public static Scanner input = new Scanner (System.in);
+	public static void q2B (Doctor [] doctors) { 
+		int count = 0; double sum = 0;
+		for (int i = 0; i < doctors.length; i++) {
+			System.out.println("enter rate for "+doctors[i].getName());
+			double r = input.nextDouble();
+			while (r != -1) {
+				sum += r;
+				count++;
+				System.out.println("enter rate for "+doctors[i].getName());
+				r = input.nextDouble();
+			}
+			doctors[i].setRate(sum/count);
+			sum = 0;
+			count = 0;
+		}
+	}
+	public static void main(String[] args) {
+		Doctor a = new Doctor ("Ron","Nose",0);
+		Doctor b = new Doctor ("Gal","eye",0);
+		Doctor [] arr = {a,b};
+		q2B(arr);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i].getRate());
+		}
+	}
+
 }
